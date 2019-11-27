415b26710e75cef17dac491a1e8cc8be2f12bb75
Emilio Araos <ea3dev@gmail.com>
2019-11-26 22:40:09 -0700

    setting up repo

---
+++ src/dev/ea3/Main.java
@@ -0,0 +1,54 @@
+package dev.ea3;
+
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int [] intArray = {20, 35, -15, 7, 55, 1, -22};
+
+        System.out.println("Unsorted array: " + Arrays.toString(intArray));
+
+        mergeSort(intArray, 0, intArray.length);
+        System.out.println("Sorted array: " + Arrays.toString(intArray));
+
+}
+
+    public static void mergeSort(int [] input, int start, int end){
+
+        if ( end - start < 2){
+            return;
+        }
+
+        int mid = (start + end) / 2;  //3 //1
+//        System.out.println(mid);
+
+        mergeSort(input, start, mid);
+        mergeSort(input, mid, end);
+        merge(input, start, mid, end);
+    }
+
+    public static void merge(int [] input, int start, int mid, int end){
+
+        if(input[mid - 1] <= input[mid]){
+            return;
+        }
+
+        int i = start;
+        int j = mid;
+        int tempIndex = 0;
+
+        int [] temp = new int[end - start];
+
+        while(i < mid && j < end){
+            temp[tempIndex++] = input[i] <= input[j] ? input[i++] : input[j++];
+        }
+
+        System.arraycopy(input, i, input, start + tempIndex, mid - i);
+        System.arraycopy(temp, 0, input, start, tempIndex);
+
+    }
+
+
+}
